6fb52aebdae5a5642b4f697b451b2d996d2b8c85
Abraklion <abraklion@gmail.com>
2021-04-10 12:15:00 +0500

    Таск: Html

---
+++ gulp/tasks/html.js
@@ -0,0 +1,26 @@
+// Переносит html файлы в папку dist
+
+module.exports = function () {
+
+  $.gulp.task('html', () => {
+
+    $.panini.refresh();
+
+    return $.gulp.src($.config.paths.html)
+      .pipe($.gp.plumber())
+      .pipe($.panini({
+        root: 'src/',
+        layouts: 'src/templates/layouts/',
+        partials: 'src/templates/partials/',
+        helpers: 'src/templates/helpers/',
+        data: 'src/templates/data/'
+      }))
+      .pipe($.gp.htmlBeautify({
+        "indent_size": 4,
+        "max_preserve_newlines": 1
+      }))
+      .pipe($.gp.if($.config.toggle.fullHtml, $.gp.htmlmin({ collapseWhitespace: true })))
+      .pipe($.gulp.dest($.config.output.path))
+      .pipe($.browserSync.stream());
+  });
+}
