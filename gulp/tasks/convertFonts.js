e19c704a0cfc96971c1046866e78d2d13847aa6f
Abraklion <abraklion@gmail.com>
2021-04-12 10:55:35 +0500

    Таск: fonts/convertFonts (редактирования)

--- gulp/tasks/convertFonts.js
+++ gulp/tasks/convertFonts.js
@@ -1,10 +1,13 @@
 // Конвертирует шрифты .ttf в формат .ttf2woff и .ttf2woff2
 
 module.exports = function () {
-  $.gulp.task('cf', function () {
+
+  $.gulp.task('cf', () => {
+
     $.gulp.src($.config.paths.fonts + '*.ttf')
       .pipe($.gp.ttf2woff())
       .pipe($.gulp.dest($.config.paths.fonts));
+
     return $.gulp.src($.config.paths.fonts + '*.ttf')
       .pipe($.gp.ttf2woff2())
       .pipe($.gulp.dest($.config.paths.fonts));
